0a1233ea9e73db6aed498f3aad95f568deb160ea
Guille Gonzalez <guille.gonzalez@datadoghq.com>
2024-03-20 09:39:25 +0100

    Update package dependencies

--- Package.swift
+++ Package.swift
@@ -1,14 +1,18 @@
-// swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.9
 
 import PackageDescription
 import CompilerPluginSupport
 
 let package = Package(
-    name: "JSONAPI",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    name: "swift-json-api",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6),
+        .macCatalyst(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "JSONAPI",
             targets: ["JSONAPI"]
@@ -19,12 +23,10 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
+        .package(url: "https://github.com/apple/swift-syntax", "509.0.0"..<"511.0.0"),
+        .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.2.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        // Macro implementation that performs the source transformation of a macro.
         .macro(
             name: "JSONAPIMacros",
             dependencies: [
@@ -32,19 +34,13 @@ let package = Package(
                 .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
             ]
         ),
-
-        // Library that exposes a macro as part of its API, which is used in client programs.
         .target(name: "JSONAPI", dependencies: ["JSONAPIMacros"]),
-
-        // A client of the library, which is able to use the macro in its own code.
         .executableTarget(name: "JSONAPIClient", dependencies: ["JSONAPI"]),
-
-        // A test target used to develop the macro implementation.
         .testTarget(
             name: "JSONAPITests",
             dependencies: [
                 "JSONAPIMacros",
-                .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
+                .product(name: "MacroTesting", package: "swift-macro-testing"),
             ]
         ),
     ]
