2b1e277402237084b8d4c72790816622b4b19f27
Guille Gonzalez <guille.gonzalez@datadoghq.com>
2024-06-13 17:45:18 +0200

    Add JSONAPIDecodingError documentation

--- Sources/JSONAPI/JSONAPIDecodingError.swift
+++ Sources/JSONAPI/JSONAPIDecodingError.swift
@@ -1,5 +1,10 @@
 import Foundation
 
+/// An error that occurs during the decoding of a JSON:API document.
 public enum JSONAPIDecodingError: Error {
+	/// Indicates that the decoder found an unhandled resource type when decoding a type annotated with the
+	/// ``ResourceUnion()`` macro.
+	///
+	/// As associated values this case contains the union type and the unhandled resource type string.
 	case unhandledResourceType(any Any.Type, String)
 }
